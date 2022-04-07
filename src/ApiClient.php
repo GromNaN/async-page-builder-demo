990cb14c68c48020de669813ff8e5a46de836166
Jérôme Tamarelle <jerome@tamarelle.net>
2022-04-08 01:14:20 +0200

    Use amphp/http-client v5, fully async with PHP fibers

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -2,26 +2,31 @@
 
 namespace App;
 
-use Symfony\Contracts\HttpClient\HttpClientInterface;
-use function Amp\delay;
+use Amp\Http\Client\HttpClient;
+use Amp\Http\Client\HttpClientBuilder;
+use Amp\Http\Client\Request;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 class ApiClient
 {
+    private HttpClient $ampClient;
+
     public function __construct(
-        private readonly HttpClientInterface $httpClient,
         private readonly string $endpoint,
+        private readonly Stopwatch $stopwatch,
     ) {
+        $this->ampClient = HttpClientBuilder::buildDefault();
     }
 
     public function get(string $path): array
     {
-        $response = $this->httpClient->request('GET', $path, [
-            'base_uri' => $this->endpoint,
-        ]);
+        $event = $this->stopwatch->start($path, 'api_client');
+        try {
+            $response = $this->ampClient->request(new Request($this->endpoint . $path));
 
-        // Wait for the next tick
-        delay(0);
-
-        return $response->toArray()['data'];
+            return json_decode($response->getBody()->buffer(), true)['data'];
+        } finally {
+            $event->stop();
+        }
     }
 }
